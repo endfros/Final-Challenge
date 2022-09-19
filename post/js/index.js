ffa0b5e50985ff5aa5ff1eba80de1fd24b996c1c
Rodrigo Montoya <montoyar32@gmail.com>
2022-09-19 11:08:17 -0500

    Added img input on post

--- post/js/index.js
+++ post/js/index.js
@@ -6,6 +6,10 @@ const title = document.querySelector("#title");
 const tags = document.querySelector("#tags");
 const content = document.querySelector("#content");
 
+const imgContainer = document.querySelector('#display-image');
+
+imgContainer.style.display = 'none';
+
 if (params.has("id")) {
   console.log(params.has("id"));
   const doc = await getPost(params.get("id"));
@@ -34,3 +38,33 @@ add.addEventListener("click", (event) => {
   let redirect = () => window.location.assign("../index.html");
   setTimeout(redirect, 1100);
 });
+
+const image_input = document.querySelector("#image-input");
+const imageBtn = document.querySelector(".add-img");
+const changeBtn = document.querySelector(".change-img");
+const removeBtn = document.querySelector('.remove-img');
+removeBtn.style.display = 'none';
+changeBtn.style.display = 'none';
+
+image_input.addEventListener("change", function() {
+  let reader = new FileReader();
+  reader.addEventListener("load", () => {
+    console.log(typeof reader.result)
+    imgContainer.style.display = 'inline-block';
+    imageBtn.style.display = 'none';
+    changeBtn.style.display = 'inline-block';
+    removeBtn.style.display = 'inline-block';
+    const uploaded_image = reader.result;
+    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+
+  reader.readAsDataURL(this.files[0]);
+});
+
+
+removeBtn.addEventListener('click',() => {
+  imgContainer.style.display = 'none';
+  imageBtn.style.display = 'inline-block';
+  changeBtn.style.display = 'none';
+  removeBtn.style.display = 'none';
+})
